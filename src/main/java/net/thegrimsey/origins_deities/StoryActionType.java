1d5c63fbac9c746346f38171a0b5116242e9f1c4
TheGrimsey <TheGrimsey@users.noreply.github.com>
2024-06-18 23:05:59 +0200

    Add hidden_from_shadows tag & expand ingenuity_ignored

--- src/main/java/net/thegrimsey/origins_deities/StoryActionType.java
+++ src/main/java/net/thegrimsey/origins_deities/StoryActionType.java
@@ -3,7 +3,7 @@ package net.thegrimsey.origins_deities;
 public enum StoryActionType {
     KILLED_HOSTILE(1.0f, StoryConstants.KILL_TRANSLATION_KEY, StoryConstants.KILL_FINAL_NAMES_COUNT, StoryEffectType.NEGATIVE_EFFECTS),
     KILLED_PLAYER(1.0f, StoryConstants.KILL_TRANSLATION_KEY, StoryConstants.KILL_FINAL_NAMES_COUNT, StoryEffectType.NEGATIVE_EFFECTS),
-    KILLED_BOSS(4.0f, StoryConstants.KILL_TRANSLATION_KEY, StoryConstants.KILL_FINAL_NAMES_COUNT, StoryEffectType.NEGATIVE_EFFECTS),
+    KILLED_BOSS(3.0f, StoryConstants.KILL_TRANSLATION_KEY, StoryConstants.KILL_FINAL_NAMES_COUNT, StoryEffectType.NEGATIVE_EFFECTS),
     FROZEN(1.0f, StoryConstants.FROZEN_TRANSLATION_KEY, StoryConstants.FROZEN_FINAL_NAMES_COUNT, StoryEffectType.COLD_EFFECTS),
     BRED_ANIMAL(1.0f, StoryConstants.HEALING_TRANSLATION_KEY, StoryConstants.HEALING_FINAL_NAMES_COUNT, StoryEffectType.POSITIVE_EFFECTS),
     BONEMEAL(1.0f, StoryConstants.HEALING_TRANSLATION_KEY, StoryConstants.HEALING_FINAL_NAMES_COUNT, StoryEffectType.POSITIVE_EFFECTS);
